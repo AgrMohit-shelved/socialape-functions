b543f0a66e0bbb88ee65142f2eedaa4955cccd9b
Mohit Raj <agrmohit@outlook.com>
2020-05-15 21:42:29 +0530

    Add user sign up and put user details in firestore

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const firebase = require('firebase');
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJDXOTvkoRflSMjchZGj3neTn2oXXZeOU',
+  authDomain: 'socialape-by-agrmohit.firebaseapp.com',
+  databaseURL: 'https://socialape-by-agrmohit.firebaseio.com',
+  projectId: 'socialape-by-agrmohit',
+  storageBucket: 'socialape-by-agrmohit.appspot.com',
+  messagingSenderId: '547764004286',
+  appId: '1:547764004286:web:640ce7ae7678314cd205ef',
+  measurementId: 'G-ENRSEP22DD',
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-  admin
-    .firestore()
-    .collection('screams')
+  db.collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
     .then((data) => {
@@ -27,7 +37,6 @@ app.get('/screams', (req, res) => {
     .catch((err) => console.error(err));
 });
 
-// eslint-disable-next-line consistent-return
 app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
@@ -35,9 +44,7 @@ app.post('/scream', (req, res) => {
     createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection('screams')
+  db.collection('screams')
     .add(newScream)
     .then((doc) =>
       res.json({ message: `document ${doc.id} created successfully` }),
@@ -48,6 +55,52 @@ app.post('/scream', (req, res) => {
     });
 });
 
-// https://baseurl.com/api/
+// Signup Route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
+  //TODO: validate user
+  let token, userId;
+  db.doc(`users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'This hande is already taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((_token) => {
+      token = _token;
+      const userCreadentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId,
+      };
+      return db.doc(`users/${newUser.handle}`).set(userCreadentials);
+    })
+    .then(() => res.status(201).json({ token }))
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already registered' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+// https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
