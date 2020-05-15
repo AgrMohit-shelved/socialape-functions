d1a19f67ed4bfa6179ffbebeb00eec0aa57adcad
Mohit Raj <agrmohit@outlook.com>
2020-05-15 17:32:31 +0530

    Add file 'dbschema.js' to store database schema

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user#...',
+      body: 'scream body',
+      createdAt: '2020-05-15T11:29:05.468Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
