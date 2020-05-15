0c6f1f8338cb2ebc618285475699eb8340db9a60
Mohit Raj <agrmohit@outlook.com>
2020-05-15 17:33:57 +0530

    Remove 'helloWorld' function and add single api endpoint'

--- functions/index.js
+++ functions/index.js
@@ -3,36 +3,36 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello World!');
-});
+const express = require('express');
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get('/screams', (req, res) => {
   admin
     .firestore()
     .collection('screams')
+    .orderBy('createdAt', 'desc')
     .get()
     .then((data) => {
       let screams = [];
-      data.forEach((doc) => screams.push(doc.data()));
+      data.forEach((doc) =>
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userhandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        }),
+      );
       return res.json(screams);
     })
     .catch((err) => console.error(err));
 });
 
 // eslint-disable-next-line consistent-return
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(400).json({ error: 'method not allowed' });
-  }
-
+app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
 
   admin
@@ -47,3 +47,7 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+// https://baseurl.com/api/
+
+exports.api = functions.https.onRequest(app);
