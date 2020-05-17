ded3ceb1b4c2b9f7dd776bf4984870abc8b9e4db
Mohit Raj <agrmohit@outlook.com>
2020-05-18 00:50:38 +0530

    Add some more data to 'dbschema.json'

--- dbschema.js
+++ dbschema.js
@@ -21,3 +21,27 @@ let db = {
     },
   ],
 };
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'dh23ggj5h32g543j5gf43',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+    bio: 'Hello, my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'London, UK',
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh7O5oWfWucVzGbHH2pa',
+    },
+    {
+      userHandle: 'user',
+      screamId: '3IOnFoQexRcofs5OhBXO',
+    },
+  ],
+};
