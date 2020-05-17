e415410609ebdf672d6baad5e2be388c408ac9ce
Mohit Raj <agrmohit@outlook.com>
2020-05-17 23:36:33 +0530

    Update 'dbschema.json' which stores database schema

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,23 @@
 let db = {
+  users: [
+    {
+      userId: 'dh23ggj5h32g543j5gf43',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'London, UK',
+    },
+  ],
   screams: [
     {
-      userHandle: 'user#...',
-      body: 'scream body',
-      createdAt: '2020-05-15T11:29:05.468Z',
+      userHandle: 'user',
+      body: 'This is a sample scream',
+      createdAt: '2019-03-15T10:59:52.798Z',
       likeCount: 5,
-      commentCount: 2,
+      commentCount: 3,
     },
   ],
 };
